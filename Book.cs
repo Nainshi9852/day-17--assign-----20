9392272c7c0bfadc09e0d400335ec6aa502b20e7
nainshisingh985@gmail.com <nainshisingh985@gmail.com>
2023-08-01 15:32:37 +0530

    First Commit

---
+++ Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_17_assign_20
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public bool IsAvailable { get; set; }
+
+    }
+}
